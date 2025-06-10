acde4a0586d6d1b8f1a5dc8d237f77df1d632d28
honkai-yan <peter-adams@qq.com>
2025-06-10 14:02:33 +0800

    优化了token对象的解析逻辑

--- backend/src/utils/token.ts
+++ backend/src/utils/token.ts
@@ -6,8 +6,10 @@ export const getTokenObj = async <T>(
   key: string,
 ): Promise<null | T> => {
   if (!req) return null;
+  if (!req.cookies) return null;
   if (!Object.prototype.hasOwnProperty.call(req.cookies, key)) return null;
   const token = (req.cookies as any)[key];
+  if (!token) return null;
   const verifiedToken: T = (await verifyToken(token)) as any;
   return verifiedToken || null;
 };
