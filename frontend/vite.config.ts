70b76aed227f0e0f0120beafb2f86645970d9406
honkai-yan <peter-adams@qq.com>
2025-10-24 10:51:00 +0800

    feat: 为前端开发服务器添加https支持

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import sslPlugin from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), sslPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       lodash: "lodash-es",
     },
+  },
+  server: {
+    https: {},
+    port: 5173,
+    host: "0.0.0.0",
   },
 });
