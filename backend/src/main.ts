b26660f02165fbc49587b9e8f9390a765565269c
honkai-yan <peter-adams@qq.com>
2025-10-24 10:40:35 +0800

    feat: 为后端添加https支持

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,18 +3,26 @@ import { AppModule } from './modules/app.module';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 async function bootstrap() {
+  const httpsOptions = {
+    key: readFileSync(path.join(__dirname, '../secrets/localhost.key')),
+    cert: readFileSync(path.join(__dirname, '../secrets/localhost.crt')),
+  };
+
   // 创建 nest 应用
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
+    httpsOptions,
   });
 
   // 全局启用CORS
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With',
